f5bc625efe99b7c2e70d9b5d8b9d2a9f6adac763
kent <kent.26pg@gmail.com>
2026-08-04 09:11:34 +0200

    added useEffect to fetch cat facts from API

--- vite-project/src/components/Catfacts/catFacts.jsx
+++ vite-project/src/components/Catfacts/catFacts.jsx
@@ -1,16 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function CatFacts() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [facts, setFacts] = useState([]);
 
+  useEffect(() => {
+    const fetchFacts = async () => {
+      try {
+        const response = await fetch("https://catfact.ninja/facts?limit=5");
 
-  return (
-    <div>
-      {}
-    </div>
-  );
+        if (!response.ok) {
+          throw new Error("Noe gikk galt med forespørselen");
+        }
+
+        const data = await response.json();
+        setFacts(data.data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchFacts();
+  }, []);
+
+  return <div>{}</div>;
 }
 
 export default CatFacts;
