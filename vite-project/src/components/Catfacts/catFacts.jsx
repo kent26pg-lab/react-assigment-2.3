2d28b7b751ea2f49c987f8a717d08666014bd98d
kent <kent.26pg@gmail.com>
2026-08-05 13:38:02 +0200

    add initial welcome message and fetch facts on click

--- vite-project/src/components/Catfacts/catFacts.jsx
+++ vite-project/src/components/Catfacts/catFacts.jsx
@@ -2,32 +2,36 @@ import { useState, useEffect } from "react";
 import styles from "./CatFacts.module.css";
 
 function CatFacts() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [facts, setFacts] = useState(null);
-
-  const fetchFacts = async () => {
-    setLoading(true);
-    setError(null);
-    try {
-      const response = await fetch("https://catfact.ninja/fact", {});
-
-      if (!response.ok) {
-        throw new Error("Noe gikk galt med forespørselen");
-      }
-
-      const data = await response.json();
-      setFacts(data);
-    } catch (err) {
-      setError(err.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const [facts, setFacts] = useState({ fact: "Trykk på meg for katte fakta" });
+  const [clickCount, setClickCount] = useState(0);
 
   useEffect(() => {
+    if (clickCount === 0) return;
+    console.log(clickCount)
+
+    const fetchFacts = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch("https://catfact.ninja/fact");
+
+        if (!response.ok) {
+          throw new Error("Noe gikk galt med forespørselen");
+        }
+
+        const data = await response.json();
+        setFacts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchFacts();
-  }, []);
+  }, [clickCount]);
 
   return (
     <div className={styles.container}>
@@ -47,7 +51,7 @@ function CatFacts() {
         src="Garfield.png"
         alt="Picture of a cat"
         className={styles.img}
-        onClick={fetchFacts}
+        onClick={() => setClickCount((prev) => prev + 1)}
       />
     </div>
   );
