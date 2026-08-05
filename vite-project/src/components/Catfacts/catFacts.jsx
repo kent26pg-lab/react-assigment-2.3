2b56ec6bcec2d870741775f210c0200eef3c5fae
kent <kent.26pg@gmail.com>
2026-08-05 12:56:05 +0200

    added the locations of the speech bubble

--- vite-project/src/components/Catfacts/catFacts.jsx
+++ vite-project/src/components/Catfacts/catFacts.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import styles from "./CatFacts.module.css"
+import styles from "./CatFacts.module.css";
 
 function CatFacts() {
   const [loading, setLoading] = useState(true);
@@ -10,9 +10,7 @@ function CatFacts() {
     setLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://catfact.ninja/fact", {
-    
-      });
+      const response = await fetch("https://catfact.ninja/fact", {});
 
       if (!response.ok) {
         throw new Error("Noe gikk galt med forespørselen");
@@ -33,11 +31,25 @@ function CatFacts() {
 
   return (
     <div className={styles.container}>
-    {loading && <p>Loading... </p> }
-    {error && <p className={styles.error}>Error: {error}</p>}
-    {!loading && !error && <p className={styles.fact}>{facts.fact}</p>}
-    <img src="Garfield.png" alt="Picture of a cat" className={styles.img} onClick={fetchFacts}/>
-  </div>
+      {error ? (
+        <p className={styles.error}>Error: {error}</p>
+      ) : (
+        <div className={styles.fact}>
+          {loading ? (
+            <div className={styles.loading}></div>
+          ) : (
+            <p>{facts.fact}</p>
+          )}
+        </div>
+      )}
+
+      <img
+        src="Garfield.png"
+        alt="Picture of a cat"
+        className={styles.img}
+        onClick={fetchFacts}
+      />
+    </div>
   );
 }
 
